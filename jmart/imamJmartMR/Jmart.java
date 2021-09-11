23ea5553a684089c1739fd31974901f633e633e8
Imam Azka <imam.azka@ui.ac.id>
2021-09-11 10:53:56 +0700

    setup

--- jmart/imamJmartMR/Jmart.java
+++ jmart/imamJmartMR/Jmart.java
@@ -1,35 +1,40 @@
 package imamJmartMR;
 
-
-/**
- * Write a description of class Jmart here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class Jmart
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class Jmart
-     */
-    public Jmart()
-    {
-        // initialise instance variables
-        x = 0;
+    public static void main(String args[]){
+        
+    }
+    
+    public int getPromo(){
+        return 0;
+    }
+    
+    public String getCostumer(){
+        return "oop";
+    }
+    
+    public float getDiscountPercentage(int before, int after){
+        if (before<after){
+            return 0;
+        }
+        else{
+            return (before-after)/10;
+        }
+        
+    }
+    
+    public int getDiscountedPrice(int price, float discountPercentage){
+        if (discountPercentage > 100){
+            return price;
+        }
+        else{
+            float num = price - (price * discountPercentage);
+            return num;
+        }
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public int getOriginalPrice(int discountedPrice, float discountPercentage){
+        
     }
 }
