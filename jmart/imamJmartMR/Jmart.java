2ae7ea38aeb29b850bbdb783c8ff58cb4e3e1f13
Imam Azka <imam.azka@ui.ac.id>
2021-09-11 11:15:44 +0700

    modul 1

--- jmart/imamJmartMR/Jmart.java
+++ jmart/imamJmartMR/Jmart.java
@@ -16,7 +16,7 @@ public class Jmart
     
     public float getDiscountPercentage(int before, int after){
         if (before<after){
-            return 0;
+            return 0.0f;
         }
         else{
             return (before-after)/10;
@@ -30,11 +30,40 @@ public class Jmart
         }
         else{
             float num = price - (price * discountPercentage);
-            return num;
+            return (int)num;
         }
     }
 
     public int getOriginalPrice(int discountedPrice, float discountPercentage){
-        
+        if (discountPercentage>100){
+            return 0;
+        }
+        else{
+            float num = discountedPrice + (discountPercentage * discountedPrice);
+            return (int)num;
+        }
+    }
+    
+    public float getCommissionMultiplier(){
+        return 0.05f;
+    }
+    
+    public int getAdjustedPrice(int price){
+        if(price<=0){
+            return 0;
+        }
+        else{
+            return price + 5;
+        }
+    }
+    
+    public int getAdminFee(int price){
+        if(price<=0){
+            return 0;
+        }
+        else{
+            float num = price * 0.05f;
+            return (int)num;
+        }
     }
 }
