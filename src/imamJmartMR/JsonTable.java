29d40f81911ad308c3393cf8e1c203c197c1dc87
Imam Azka <imam.azka@ui.ac.id>
2021-11-13 11:26:12 +0700

    CS - Modul 6 - Wadaw

--- src/imamJmartMR/JsonTable.java
+++ src/imamJmartMR/JsonTable.java
@@ -8,26 +8,31 @@ import java.util.Vector;
 import java.io.File;
 import com.google.gson.*;
 
-public class JsonTable <T>{
+public class JsonTable <T extends Vector>{
 
     public static String filepath;
     private static final Gson gson = new Gson();
 
-    public JsonTable (Class<T> clazz, String filepath) throws IOException {
+    public JsonTable (Class<T> clazz, String filepath) throws IOException{
 
         this.filepath = filepath;
         Class<T[]> array = (Class<T[]>) Array.newInstance(clazz, 0).getClass();
-        T[] loaded = readJson(array, filepath);
+        T[] read = readJson(array, filepath);
     }
 
-    public static<T> T readJson (Class<T> clazz, String filepath) throws IOException {
+    public static<T> T readJson (Class<T> clazz, String filepath) throws FileNotFoundException {
 
         clazz = gson.fromJson(filepath, clazz.getClass());
         return null;
     }
 
-    public void writeJson () throws IOException {
-            //gson.toJson(JsonTable<T>, JsonTable<T>getClass());
+    public void writeJson () {
+        try {
+            FileWriter writer = new FileWriter(filepath);
+            gson.toJson(writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void writeJson (Object object, String filepath) {
